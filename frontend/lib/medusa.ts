f25d626504d2765b55161bbab11b5da644bf3406
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2025-10-12 13:36:59 +0200

    Added publishableKey to medusa client and allowed localhost images in Nextjs

--- frontend/lib/medusa.ts
+++ frontend/lib/medusa.ts
@@ -1,7 +1,8 @@
 import Medusa from "@medusajs/js-sdk";
 
 const medusa = new Medusa({
-    baseUrl: process.env.PUBLIC_MEDUSA_URL || "http://localhost:9000"
+    baseUrl: process.env.PUBLIC_MEDUSA_URL || "http://localhost:9000",
+    publishableKey: process.env.MEDUSA_API_KEY,
 })
 
 export default medusa;
